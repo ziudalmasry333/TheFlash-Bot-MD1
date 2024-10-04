6781eddbb5112592dd2b537ad06890100b2d0072
ziudalmasry333 <zziad3077@gmail.com>
2024-10-04 21:21:34 +0300

    Update لينك.js

--- plugins/لينك.js
+++ plugins/لينك.js
@@ -3,10 +3,10 @@ const handler = async (m, {conn, args}) => {
   const group = m.chat;
   conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
     contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null,
-      title: 'لينك الجروب',
+      title: 'لينك الشات بتاع المطور',
       body: '𝐅𝐋𝐀𝐒𝐇﹝⚡﹞𝐁𝐎𝐓',
       previewType: 0, thumbnail: fs.readFileSync('./Menu.png'),
-      sourceUrl: `https://whatsapp.com/channel/0029ValRCUIFi8xmK9xBOr0I`}}});
+      sourceUrl: `https://wa.me/201223010181`}}});
 };
 handler.help = ['linkgroup'];
 handler.tags = ['group'];
