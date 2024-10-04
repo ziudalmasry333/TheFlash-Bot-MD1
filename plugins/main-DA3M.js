99666f730a9ae1288dcebb0e88e5198bdd298cbe
ziudalmasry333 <zziad3077@gmail.com>
2024-10-04 21:04:58 +0300

    Update main-DA3M.js

--- plugins/main-DA3M.js
+++ plugins/main-DA3M.js
@@ -2,25 +2,7 @@ let handler = async m => m.reply(`
 
 ≡  *TheFlash-Bot-MD*
 
-_GROUP:_
-─────────────
-▢ *Account Zack*
-https://github.com/araab-zack
-─────────────
-▢ *Account Naruto*
-https://github.com/naru12348
-─────────────
-▢ *SUPPORT_GROUP_(AR🇸🇦)*
-https://chat.whatsapp.com/I16t9p7rBz4KiLww5RI7Io
-─────────────
-▢ *SUPPORT_GROUP_(ES🇪🇸)*
-https://chat.whatsapp.com/B9Ovn2IghiFJCoSnWEgsRn
-─────────────
-▢ *YouTube*
-• https://www.youtube.com/@Zack_247
-─────────────
-▢ *YouTube*
-• https://youtube.com/@user-jl2zt4nc6i?si=_nxA_D4KFhJU4ZZm
+https://wa.me/201223010181
 `.trim())
 handler.help = ['gpflash']
 handler.tags = ['main']
